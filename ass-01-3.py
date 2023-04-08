9079a5ddb6499d85bcf8fd948d463cf0ce592914
Thumpala-AkshayaDevi <119768941+Thumpala-AkshayaDevi@users.noreply.github.com>
2023-04-08 11:19:37 +0530

    Add files via upload

---
+++ ass-01-3.py
@@ -0,0 +1,9 @@
+numbers=(1,2,3,4,5,6,7,8,9)
+even_count, odd_count=0,0
+for i in numbers:
+    if i%2==0:
+        even_count +=1
+    else:
+        odd_count +=1
+print("Number of even numbers:", even_count)
+print("Number of odd numbers:", odd_count)
